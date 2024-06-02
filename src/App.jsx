1dfac6c26fce781f2006d1f52a79fa00ddb9568b
BasselZeidan80 <BasselZeidan80@gmail.com>
2024-06-02 12:01:38 +0300

    Upload Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
 import "./App.css";
 
-import { RouterProvider, createBrowserRouter, createHashRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createHashRouter,
+} from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import Home from "./Components/Home/Home";
 import Register from "./Components/Register/Register";
 import Login from "./Components/Register/Login";
 import ProtectedRoutes from "./ProtectedRoutes/ProtectedRoutes.jsx";
 import { UserProfileProvider } from "./Context/UserProfile/User.jsx";
+import NotFound from "./Components/NotFound/NotFound.jsx";
 function App() {
   let routes = createHashRouter([
     {
       path: "/",
       element: <Layout />,
       children: [
-        { path: "home", element:  <ProtectedRoutes><Home />  </ProtectedRoutes>  },
-        { index: true, element:  <ProtectedRoutes><Home />  </ProtectedRoutes>  },
+        {
+          path: "home",
+          element: (
+            <ProtectedRoutes>
+              <Home />{" "}
+            </ProtectedRoutes>
+          ),
+        },
+        {
+          index: true,
+          element: (
+            <ProtectedRoutes>
+              <Home />{" "}
+            </ProtectedRoutes>
+          ),
+        },
         { path: "Register", element: <Register /> },
         { path: "login", element: <Login /> },
-        { path: "*", element: <h1>NotFound</h1> },
-
+        { path: "*", element: <NotFound /> },
       ],
     },
   ]);
   return (
     <>
-    <UserProfileProvider>
-      <RouterProvider router={routes}></RouterProvider>
+      <UserProfileProvider>
+        <RouterProvider router={routes}></RouterProvider>
       </UserProfileProvider>
     </>
   );
--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import styles from "./NotFound.module.css";
+import "./NotFound.css";
 export default function NotFound() {
   return (
     <>
-      <h2>NotFound</h2>
+      <div className="container flex-column notFnd d-flex align-items-center justify-content-center  vh-100 my-4">
+        <h1>404</h1>
+        <p>Not found</p>
+      </div>
     </>
   );
 }
